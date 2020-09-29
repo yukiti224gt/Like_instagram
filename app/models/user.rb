580e6d40dc367548303f39d6264cf3b73f7a51e9
yukiti224gt <mamekiti084@gmail.com>
2020-09-29 14:51:54 +0900

    バリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :name, presence :true, length: { maximum : 50}
+  # vadidatesとは要約すると入力チェックのこと（DBに保存される前のチェック）
 end
