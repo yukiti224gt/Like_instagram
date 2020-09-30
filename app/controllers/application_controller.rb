62231d5c44dd4c98b138796bd83641aba34629c4
yukiti224gt <mamekiti084@gmail.com>
2020-09-30 10:16:10 +0900

    ビュー作成途中

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+  # configure_permitted_parametersメソッドはdevise_controllerを用いる時しか処理しない意味
 
   protected
 
