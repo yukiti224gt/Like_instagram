8449bce5aab47a7936143160ab7a29b690f90374
yukiti224gt <mamekiti084@gmail.com>
2020-10-01 12:13:11 +0900

    application_helper.rbへprofile_photoカラムへの処理を記入

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_29_054343) do
+ActiveRecord::Schema.define(version: 2020_10_01_015327) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
