0e89816e6e48794168641c5588024758382e633b
yukiti224gt <mamekiti084@gmail.com>
2020-10-01 10:24:14 +0900

    usersコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # pagesコントローラーのhomeアクションのルーティングを追加する
   root 'pages#home'
+
+  get "/users/id",to: "users#show", as: "user"
+  # asオプションを用いるとルーティングへ名前を付けれる。今回userという名前をつけたためuser_pathメソッドが生成される。
+  
+
 end
