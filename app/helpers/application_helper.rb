b022f1e3ce3eb9c97c2f73194553d8c32593754b
yukiti224gt <mamekiti084@gmail.com>
2020-10-07 12:51:57 +0900

    プロフィールリンクの限定表示

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,6 @@ module ApplicationHelper
   def avatar_url(user)
     return user.profile_photo unless user.profile_photo.nil?
     gravatar_id = Digest::MD5::hexdigest(user.email).downcase
-    "https://www.gravarar.com/avatar/#{gravatar_id}.jpg"
+    "https://www.gravatar.com/avatar/#{gravatar_id}.jpg"
     end
 end
