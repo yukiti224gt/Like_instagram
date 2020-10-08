97a1eac78787b6dc05c7bc4a40b998721d374988
yukiti224gt <mamekiti084@gmail.com>
2020-10-08 12:51:59 +0900

    user.rbへメソッドを追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,17 @@ class User < ApplicationRecord
   
   validates :name, presence: true, length: { maximum: 50 }
   # vadidatesとは要約すると入力チェックのこと（DBに保存される前のチェック）
+  
+  def update_without_current_password(params, *options)
+    params.delete(:current_password)
+
+    if params[:password].blank? && params[:password_confirmation].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation)
+    end
+
+    result = update_attributes(params, *options)
+    clean_up_passwords
+    result
+  end
 end
