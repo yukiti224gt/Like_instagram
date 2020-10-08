f6733eaac35e2dc0257fef3e8da42081b0b2c545
yukiti224gt <mamekiti084@gmail.com>
2020-10-08 15:16:32 +0900

    コントローラーを追加

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,4 +5,8 @@ class RegistrationsController < Devise::RegistrationsController
   def update_resource(resouce, params)
     resouce.update_without_current_password(params)
   end
+
+  def after_update_path_for(resouce)
+    user_path(resouce)
+  end
 end
