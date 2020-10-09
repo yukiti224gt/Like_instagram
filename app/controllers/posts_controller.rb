a54da2d685655430634d63427c6497b7fa35eabd
yukiti224gt <mamekiti084@gmail.com>
2020-10-09 11:47:19 +0900

    なんかよくわからんけどposts_cotrollerへ追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,25 @@
 class PostsController < ApplicationController
+
+  def new
+    @post = Post.new
+    @post.photos.build
+    # new,buildはどちらもインスタンスを作るメソッドのこと
+
+  def create
+    @post = Post.new(post_params)
+    if @post.photos.present?
+      @post.save
+      redirect_to root_path
+      flash[:notice] = "投稿が保存されました"
+    else
+      redirect_to root_path
+      flash[:alert] = "投稿が失敗しました"  
+    end
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:caption,photos_attributes: [:image])
+      .merge(user_id: current_user.id)
+    end
 end
