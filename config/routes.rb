0862f182721cc4d762084ddc5f9c0c8d425c59c0
yukiti224gt <mamekiti084@gmail.com>
2020-10-12 17:36:04 +0900

    rootルーティングを投稿画面から初期画面へ変更

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   devise_for :users,
     controllers: { registrations: "registrations" }
   # pagesコントローラーのhomeアクションのルーティングを追加する
-  root 'pages#home'
+  root 'posts#index'
 
   get "/users/:id",to: "users#show", as: "user"
   
-  resources :posts, only: %i(new create index) do
+  resources :posts, only: %i(index new create) do
     resources :photos, only: %i(create)
   end
   # asオプションを用いるとルーティングへ名前を付けれる。今回userという名前をつけたためuser_pathメソッドが生成される。
