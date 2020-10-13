9fa37d211a25a8636de3d79eaff683d6565d23a3
yukiti224gt <mamekiti084@gmail.com>
2020-10-13 15:58:58 +0900

    アソシエーション設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,7 @@ class Post < ApplicationRecord
 
   has_many :photos, dependent: :destroy
 
+  has_many :likes, -> { order(created_at: :desc) }, dependent: :destroy
+  
   accepts_nested_attributes_for :photos
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-
+  has_many :likes
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
