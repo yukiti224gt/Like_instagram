823e20366bc7c99ec19d9d696ad91e9f1f37fd0e
yukiti224gt <mamekiti084@gmail.com>
2020-10-14 00:27:31 +0900

    サインインユーザのみいいね

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,8 +1,19 @@
 class LikesController < ApplicationController
+
+  before_action :authenticate_user!
+  
   def create
     @like = current_user.likes.build(like_params)
     @post = @like.post
     if @like.save
+      respond_to :js
+    end
+  end
+
+  def destroy 
+    @like = Like.find_by(id: params[:id])
+    @post = @like.post
+    if @like.destroy
       respond_to :js
     end
   end
