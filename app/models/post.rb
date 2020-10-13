16c9841e40cd3a378eb3506ba43166bd3e406667
yukiti224gt <mamekiti084@gmail.com>
2020-10-14 01:03:39 +0900

    index.htmlへlikeメソッド追加等

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,10 @@ class Post < ApplicationRecord
   has_many :photos, dependent: :destroy
 
   has_many :likes, -> { order(created_at: :desc) }, dependent: :destroy
-  
+
   accepts_nested_attributes_for :photos
+
+  def liked_by(user)
+    Like.find_by(user_id: user.id, post_id: id)
+  end
 end
