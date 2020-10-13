cde2c40b59c42fc2471cb6e7e461877688a639fa
yukiti224gt <mamekiti084@gmail.com>
2020-10-13 12:40:35 +0900

    postsコントローラーにdestroyアクションを作成

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,8 @@ Rails.application.routes.draw do
 
   get "/users/:id",to: "users#show", as: "user"
   
-  resources :posts, only: %i(index new create show) do
+  resources :posts, only: %i(index new create show destroy) do
     resources :photos, only: %i(create)
   end
   # asオプションを用いるとルーティングへ名前を付けれる。今回userという名前をつけたためuser_pathメソッドが生成される。
-  
-
 end
