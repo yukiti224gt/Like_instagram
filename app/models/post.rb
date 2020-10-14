252f80baa0e5ee57c082fb2e9e171045a1a08c7b
yukiti224gt <mamekiti084@gmail.com>
2020-10-14 10:15:15 +0900

    postとcommentのアソシエーション設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,11 @@
 class Post < ApplicationRecord
 
   belongs_to :user
-
   has_many :photos, dependent: :destroy
-
   has_many :likes, -> { order(created_at: :desc) }, dependent: :destroy
+
+  has_many :comments, dependent: :destroy
+  # dependentで関連づけられて削除される
 
   accepts_nested_attributes_for :photos
 
