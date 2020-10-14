f286381934b4cb043f0de256cdb4456beaa14a03
yukiti224gt <mamekiti084@gmail.com>
2020-10-14 12:36:30 +0900

    削除機能追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,7 @@
 class CommentsController < ApplicationController
+
+  before_acttion :authenticate_user!
+
   def create
     @comment = Comment.new(comment_params)
     @post = @comment.post
@@ -8,6 +11,16 @@ class CommentsController < ApplicationController
     else
       flash[:alert] = "コメントに失敗しました"
     end
+end
+
+def destroy
+  @comment = Comment.find_by(id: params[:id])
+  @post = @comment.post
+  if @comment.destroy
+    respond_to :js
+  else
+    flash[:alert] = "コメントの削除に失敗しました"
+  end
 end
 
 private 
