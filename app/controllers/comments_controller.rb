8bafc6cfe3a8a9a001293227d8c3b7f18d32d33d
yukiti224gt <mamekiti084@gmail.com>
2020-10-15 13:30:01 +0900

    投稿エラー修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
 
-  before_acttion :authenticate_user!
+  before_action :authenticate_user!
 
   def create
     @comment = Comment.new(comment_params)
